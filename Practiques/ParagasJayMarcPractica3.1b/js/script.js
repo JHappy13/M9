8c9a571b53948184a74c3d791bd2c65ef4e96232
JHappy13 <jparagas.daw2n23@lamerce.com>
2023-10-30 19:39:36 +0100

    Nuevos cambios

---
+++ Practiques/ParagasJayMarcPractica3.1b/js/script.js
@@ -0,0 +1,19 @@
+function openForm(){
+    window.location = "form.html";
+}
+
+function openReadMore(){
+    window.location = "readMore.html";
+}
+
+function submit(){
+    window.location = "index.html";
+}
+
+function verLista(){
+    window.location = "listaMejores.html";
+}
+
+function inicio(){
+    window.location = "index.html";
+}
